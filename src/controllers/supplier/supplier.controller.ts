6d8029678a9d175cf06f201407fdfde068b898f1
Andresh4321 <shahiandy123@gmail.com>
2026-02-16 21:20:30 +0545

    Controller for suppliers

---
+++ src/controllers/supplier/supplier.controller.ts
@@ -0,0 +1,118 @@
+import { Request, Response } from "express";
+import { SupplierService } from "../../services/supplier/supplier.service";
+
+let supplierService = new SupplierService();
+
+export class SupplierController{
+    async createSupplier(req: Request, res: Response){
+        try{
+            const newSupplier = await supplierService.createSupplier(req.body);
+            return res.status(201).json(
+                { success: true, data: newSupplier, message: "Supplier created successfully" }
+            )
+        }catch(error: Error | any){
+            return res.status(error.statusCode || 500).json(
+                { success: false, message: error.message || "Internal Server Error" }
+            )
+        }
+    }
+    async getSupplierById(req: Request, res: Response){
+        try{
+            const supplier = await supplierService.getSupplierById(req.params.id);
+            return res.status(200).json(
+                { success: true, data: supplier, message: "Supplier retrieved successfully" }
+            )
+        }
+        catch(error: Error | any){
+            return res.status(error.statusCode || 500).json(
+                { success: false, message: error.message || "Internal Server Error" }
+            )
+        }
+    }
+    async getSupplierByEmail(req: Request, res: Response){
+        try{
+            const supplier = await supplierService.getSupplierByEmail(req.params.email);
+            return res.status(200).json(
+                { success: true, data: supplier, message: "Supplier retrieved successfully" }
+            )
+        }
+        catch(error: Error | any){
+            return res.status(error.statusCode || 500).json(
+                { success: false, message: error.message || "Internal Server Error" }
+            )
+        }
+    }
+    async getSupplierByName(req: Request, res: Response){
+        try{
+            const supplier = await supplierService.getSupplierByName(req.params.name);
+            return res.status(200).json(
+                { success: true, data: supplier, message: "Supplier retrieved successfully" }
+            )
+        }
+        catch(error: Error | any){
+            return res.status(error.statusCode || 500).json(
+                { success: false, message: error.message || "Internal Server Error" }
+            )
+        }
+    }
+    async getSupplierByProduct(req: Request, res: Response){
+        try{
+            const suppliers = await supplierService.getSupplierByProduct(req.params.product);
+            return res.status(200).json(
+                { success: true, data: suppliers, message: "Suppliers retrieved successfully" }
+            )
+        }
+        catch(error: Error | any){
+            return res.status(error.statusCode || 500).json(
+                { success: false, message: error.message || "Internal Server Error" }
+            )
+        }
+    }
+
+    async updateSupplierById(req: Request, res: Response){
+        try{
+            const updatedSupplier = await supplierService.updateSupplierById(req.params.id, req.body);
+            return res.status(200).json(
+                { success: true, data: updatedSupplier, message: "Supplier updated successfully" }
+            )
+        }
+        catch(error: Error | any){
+            return res.status(error.statusCode || 500).json(
+                { success: false, message: error.message || "Internal Server Error" }
+            )
+        }
+    }
+    async deleteSupplierById(req: Request, res: Response){
+        try{    
+            await supplierService.deleteSupplierById(req.params.id);
+            return res.status(200).json(
+                { success: true, message: "Supplier deleted successfully" }
+            )
+        }
+        catch(error: Error | any){
+            return res.status(error.statusCode || 500).json(
+                { success: false, message: error.message || "Internal Server Error" }
+            )
+        }
+    }
+    async getAllSuppliers(req: Request, res: Response){
+        try{
+            const page = parseInt(req.query.page as string) || 1;
+            const limit = parseInt(req.query.limit as string) || 10;
+            const { suppliers, total } = await supplierService.getAllSuppliers(page, limit);
+            return res.status(200).json(
+                { success: true, data: suppliers, total, page, totalPages: Math.ceil(total / limit), message: "Suppliers retrieved successfully" }
+            )
+
+        }       
+            catch(error: Error | any){
+            return res.status(error.statusCode || 500).json(
+                { success: false, message: error.message || "Internal Server Error" }
+            )
+        }
+    }
+}
+export default SupplierController;
+
+
+
